0bde30d4fdc66462e13a129cada26fc24e1fbb42
indominuShot <luang193@gmail.com>
2020-12-07 21:37:39 -0300

    getStaticProps e fallback adicionados

--- src/pages/Members/[login].tsx
+++ src/pages/Members/[login].tsx
@@ -1,17 +1,33 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/dist/client/router";
 
 export default function Member({ user }) {
+  const {isFallback} = useRouter();
+
+  if(isFallback){
+    return <h1>Carregando...</h1>
+  }
+
   return <div>
-    <img src={user.avatar_url} alt={user.name} width={80} style={{ borderRadius: '50%' }} />
+    <img src={user.avatar_url} alt={user.name} width={300} style={{ borderRadius: '50%' }} />
     <h1>{user.name}</h1>
     <p>{user.bio}</p>
   </div>
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('https://api.github.com/orgs/rocketseat/members');
+  const data = await response.json();
+
+  const paths: any[] = data.map(member => ({
+    params: {login:member.login}
+  }))
+
+  console.log(paths.length);
+
   return {
-    paths: [],
-    fallback: false
+    paths,
+    fallback: true
   }
 }
 
